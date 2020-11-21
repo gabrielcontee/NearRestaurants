9fb02d1a9880c1dcdac8ac22f7ddeafe6c560dd1
gabrielcontee <gabrielconte14@gmail.com>
2020-11-21 10:32:43 -0300

    Replaced. storyboard with xib

---
+++ NearRestaurants/MapViewController.swift
@@ -0,0 +1,14 @@
+//
+//  MapViewController.swift
+//  NearRestaurants
+//
+//  Created by Gabriel Figueiredo Conde on 11/21/20.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    
+
+}
