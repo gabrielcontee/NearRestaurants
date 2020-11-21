96e58af2b202fc805eb59d98ad8278c3dea7addf
gabrielcontee <gabrielconte14@gmail.com>
2020-11-21 10:07:07 -0300

    Created Xcode project

---
+++ NearRestaurants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearRestaurants
+//
+//  Created by Gabriel Figueiredo Conde on 11/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
